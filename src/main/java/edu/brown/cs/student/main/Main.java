9b3c838afeb395da7b1e4ba3cb81e3cd8dafea13
Andrew Li <Andrewli2024@gmail.com>
2022-03-03 18:25:43 -0500

    finished lab!

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -6,10 +6,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
@@ -59,9 +64,11 @@ public final class Main {
 
     // Setup Spark Routes
 
-    // TODO: create a call to Spark.post to make a POST request to a URL which
+    // create a call to Spark.post to make a POST request to a URL which
     // will handle getting matchmaking results for the input
     // It should only take in the route and a new ResultsHandler
+    Spark.post("/match", new ResultsHandler());
+
     Spark.options("/*", (request, response) -> {
       String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
       if (accessControlRequestHeaders != null) {
@@ -110,13 +117,25 @@ public final class Main {
       // TODO: Get JSONObject from req and use it to get the value of the sun, moon,
       // and rising
       // for generating matches
+      try {
+        JSONObject json = new JSONObject(req.body());
+        String sun = json.getString("sun");
+        String moon = json.getString("moon");
+        String rising = json.getString("rising");
 
-      // TODO: use the MatchMaker.makeMatches method to get matches
+        // use the MatchMaker.makeMatches method to get matches
+        List<String> matches = MatchMaker.makeMatches(sun, moon, rising);
 
-      // TODO: create an immutable map using the matches
+        // create an immutable map using the matches
+        Map<String, List<String>> matchesMap = Map.of("matches", matches);
 
-      // TODO: return a json of the suggestions (HINT: use GSON.toJson())
-      Gson GSON = new Gson();
+        // return a json of the suggestions (HINT: use GSON.toJson())
+        Gson gson = new Gson();
+        return gson.toJson(matchesMap);
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+
       return null;
     }
   }
